9bad17dc4c7d5c44c34c3931f631704c25b331fb
Nicolas Schmid <nicolas@breiten.ch>
2019-09-09 14:01:10 +0200

    update dispatcher

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from werkzeug.wsgi import DispatcherMiddleware
 from multilingual_v0_6.app import app as app06
 from multilingual_v1_0.app import app as app10
 
-application = DispatcherMiddleware(app06, {'/v10':app10})
+application = DispatcherMiddleware({'/lang0.6': app06}, {'/lang1.0':app10})
