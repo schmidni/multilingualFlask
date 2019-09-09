3618396ee10a3220d72c8702731cfeafc0da37ae
Nicolas <nicolas@breiten.ch>
2019-09-09 12:32:40 +0000

    update wsgi file with dispatcher, working

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,8 @@ from werkzeug.wsgi import DispatcherMiddleware
 from multilingual_v0_6.app import app as app06
 from multilingual_v1_0.app import app as app10
 
-application = DispatcherMiddleware({'/lang06':app06}, {'/lang10':app10})
+def null_app(environ, start_response):
+    start_response('404 Not Found', [('Content-Type', 'text/plain')])
+    return ['Not Found']
+
+application = DispatcherMiddleware(null_app, {'/lang10':app10, '/lang06':app06})
