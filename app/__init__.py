e791fa01d078d40a63fea2ac5305390087531d64
Nicolas Schmid <nicolas@breiten.ch>
2021-03-21 13:11:16 +0100

    chore: update requirements.txt and some formatting

--- app/__init__.py
+++ app/__init__.py
@@ -2,23 +2,28 @@ from flask import Flask, request, g, redirect, url_for
 from flask_babel import Babel
 from config import Config
 
+
+# import and register blueprints
+from app.blueprints.multilingual import multilingual
+
 # set up application
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# import and register blueprints
-from app.blueprints.multilingual import multilingual
 app.register_blueprint(multilingual)
 
 # set up babel
 babel = Babel(app)
+
+
 @babel.localeselector
 def get_locale():
     if not g.get('lang_code', None):
-        g.lang_code = request.accept_languages.best_match(app.config['LANGUAGES'])
+        g.lang_code = request.accept_languages.best_match(
+            app.config['LANGUAGES'])
     return g.lang_code
+
 
 @app.route('/')
 def home():
-    g.lang_code = request.accept_languages.best_match(app.config['LANGUAGES'])
     return redirect(url_for('multilingual.index'))
--- app/blueprints/multilingual/routes.py
+++ app/blueprints/multilingual/routes.py
@@ -1,24 +1,29 @@
-from flask import render_template, Blueprint, g, redirect, request, current_app, abort, url_for
-from flask_babel import _, refresh
+from flask import (render_template, Blueprint, g, redirect,
+                   request, current_app, abort, url_for)
+from flask_babel import _
 from app import app
-import os
 
-multilingual = Blueprint('multilingual', __name__, template_folder='templates', url_prefix='/<lang_code>')
+multilingual = Blueprint('multilingual', __name__,
+                         template_folder='templates', url_prefix='/<lang_code>')
+
 
 @multilingual.url_defaults
 def add_language_code(endpoint, values):
     values.setdefault('lang_code', g.lang_code)
 
+
 @multilingual.url_value_preprocessor
 def pull_lang_code(endpoint, values):
     g.lang_code = values.pop('lang_code')
+
 
 @multilingual.before_request
 def before_request():
     if g.lang_code not in current_app.config['LANGUAGES']:
         adapter = app.url_map.bind('')
         try:
-            endpoint, args = adapter.match('/en' + request.full_path.rstrip('/ ?'))
+            endpoint, args = adapter.match(
+                '/en' + request.full_path.rstrip('/ ?'))
             return redirect(url_for(endpoint, **args), 301)
         except:
             abort(404)
@@ -27,6 +32,7 @@ def before_request():
     if 'lang_code' in dfl:
         if dfl['lang_code'] != request.full_path.split('/')[1]:
             abort(404)
+
 
 @multilingual.route('/')
 @multilingual.route('/index')
@@ -43,6 +49,7 @@ def index():
         }
     ]
     return render_template('multilingual/index.html', title=_('Home'), user=user, posts=posts)
+
 
 @multilingual.route('/cake', defaults={'lang_code': 'en'})
 @multilingual.route('/kuchen', defaults={'lang_code': 'de'})
